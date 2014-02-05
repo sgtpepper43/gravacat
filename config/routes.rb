e97ffb360003b2ab1f0df3cf445bb64805fdf209
Trevor Fenn <sgtpepper43@gmail.com>
2014-02-04 23:19:12 -0700

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Gravacat::Application.routes.draw do
 
+  root  'static_pages#home'
   match '/:id', to: 'kittens#kittens', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
