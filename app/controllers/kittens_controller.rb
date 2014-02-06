d9730243e32a6c78cee4f7eaab0576c6f45699f3
Trevor Fenn <sgtpepper43@gmail.com>
2014-02-05 19:27:59 -0700

    Add random kittehs

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,7 +2,11 @@ class KittensController < ApplicationController
   def kittens
     dir = Rails.root.join('app', 'assets', 'images');
     num_images = Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }
-    kitteh = (params[:id].hash.abs % num_images) + 1
+    if params[:id] == "random" || params[:id] == "rand" || params[:id] == "r"
+      kitteh = rand(num_images) + 1
+    else
+      kitteh = (params[:id].hash.abs % num_images) + 1
+    end
     send_file Rails.root.join('app', 'assets', 'images', "#{kitteh}.png"), :type => 'image/png', :disposition => 'inline', :x_sendfile=>true
   end
 end
